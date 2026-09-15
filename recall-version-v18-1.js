7d7bf55bbebb9e57316d63d00b0f0d81ad5cd7bd
Abimael Brilhante <88044370+AbimaelBrilhante@users.noreply.github.com>
2026-09-15 12:06:56 -0300

    Set Recall version to 19.0

--- recall-version-v18-1.js
+++ recall-version-v18-1.js
@@ -2,7 +2,7 @@
   'use strict';
   if (window.__recallVersionV181) return;
   window.__recallVersionV181 = true;
-  const VERSION='18.1';
+  const VERSION='19.0';
   function enforce(){
     document.documentElement.dataset.recallAppVersion=VERSION;
     document.querySelectorAll('.rf3-version').forEach(el=>{
