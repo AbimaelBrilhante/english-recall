8548e33d7017515aec2abf2a88e06fc9d97abb71
Abimael Brilhante <88044370+AbimaelBrilhante@users.noreply.github.com>
2026-09-20 15:50:02 -0300

    Bump Recall version to 20.2

--- recall-version-v18-1.js
+++ recall-version-v18-1.js
@@ -2,7 +2,7 @@
   'use strict';
   if (window.__recallVersionV181) return;
   window.__recallVersionV181 = true;
-  const VERSION='20.1';
+  const VERSION='20.2';
   function enforce(){
     document.documentElement.dataset.recallAppVersion=VERSION;
     document.querySelectorAll('.rf3-version').forEach(el=>{
